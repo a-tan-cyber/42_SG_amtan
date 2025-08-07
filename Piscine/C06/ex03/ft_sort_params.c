9b0d9a842c0195e4fb7af633623ca0f5916f69db
amtan <amtan@student.42singapore.sg>
2025-08-07 23:41:06 +0800

    Complete C06.

---
+++ Piscine/C06/ex03/ft_sort_params.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 17:13:01 by amtan             #+#    #+#             */
+/*   Updated: 2025/08/07 23:38:19 by amtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		i;
+	char	*temp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			temp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = temp;
+			i = 1;
+		}
+		else
+			i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc < 2)
+		return (0);
+	ft_sort_params(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
